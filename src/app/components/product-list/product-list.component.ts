b178661f2656048265c1a606d0407e0636d51ccf
stephen ikani <steville2013@gmail.com>
2024-01-06 15:26:42 +0100

    list product in basic html page

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-table.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products: Product[] =[];
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void{
+    this.listProducts();
+  }
+  listProducts() {
+    const result = this.productService.getProductList().subscribe(
+      data =>{
+        this.products = data;
+      }
+    )
+    console.log("ProductListPage", result);
+    return result;
+  }
+
+}
