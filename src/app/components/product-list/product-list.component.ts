95ab3cea0992cd45f0d9dfff5952df90ce368485
stephen ikani <steville2013@gmail.com>
2024-07-04 12:54:43 +0100

    add selecting page size and adding boundary links

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -86,4 +86,10 @@ export class ProductListComponent {
 
   }
 
+  updatePageSize(pageSize: string){
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+
 }
