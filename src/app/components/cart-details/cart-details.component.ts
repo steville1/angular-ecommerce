14bbb59ce63b342b9f470d82821c39abce920530
stephen ikani <steville2013@gmail.com>
2024-08-04 23:02:39 +0100

    layout the button for increment and decrement cart quantity and add an event handler for increment

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -37,4 +37,8 @@ export class CartDetailsComponent implements OnInit {
     this.cartService.computeCartTotals();
   }
 
+  incrementQuantity(theCartItem: CartItem){
+    this.cartService.addToCart(theCartItem);
+  }
+
 }
