ae71c10111fe6da018d5e6fc6b2efda8e1e9a936
stephen ikani <steville2013@gmail.com>
2024-08-23 09:10:27 +0100

    fix bug for populate country and state, when the checkbox same for shipping is checked for billing the state is not populated for billing

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { TastefulTreasureFormService } from 'src/app/services/tasteful-treasure-form.service';
 
 @Component({
@@ -18,6 +19,8 @@ export class CheckoutComponent implements OnInit {
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
   countries: Country[] = [];
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
   
   constructor(private formBuilder: FormBuilder, private tastefulTreasureFormService: TastefulTreasureFormService) { }
 
@@ -87,9 +90,15 @@ export class CheckoutComponent implements OnInit {
     if (event.target.checked) {
       this.checkoutFormGroup.controls.billingAddress
             .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+      
+      //Bug fix for state
+      this.billingAddressStates = this.shippingAddressStates;
     }
     else {
       this.checkoutFormGroup.controls.billingAddress.reset();
+
+      //bug fix for states
+      this.billingAddressStates = [];
     }
     
   }
@@ -98,7 +107,10 @@ export class CheckoutComponent implements OnInit {
   onSubmit() {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
-    //console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
+    console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
+
+    console.log("The shipping address country is" + this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log("The shipping address state is" + this.checkoutFormGroup.get('shippingAddress').value.state.name)
   }
 
   handleMonthsAndYears() {
@@ -123,6 +135,32 @@ export class CheckoutComponent implements OnInit {
       data => {
         console.log("Retrieved credit card months: " + JSON.stringify(data));
         this.creditCardMonths = data;
+      }
+    );
+  }
+
+  getStates(formGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.tastefulTreasureFormService.getStates(countryCode).subscribe(
+      data => {
+
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data; 
+        }
+        else {
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        formGroup.get('state').setValue(data[0]);
       }
     );
   }
