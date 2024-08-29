c998cea2efab62e698f0d818527db38fff9bc631
stephen ikani <steville2013@gmail.com>
2024-08-29 18:13:53 +0100

    add okta functionality and component for login

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc:{
-        clientId: '',
-        issuer: '',
+        clientId: '0oajb067wikLm4mdw5d7',
+        issuer: 'https://dev-28811880.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
